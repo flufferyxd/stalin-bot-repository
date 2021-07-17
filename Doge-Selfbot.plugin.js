6db83eaf849d518ab6491cbb083ab4fbdfd49590
FlufferyAutism <74445505+FlufferyAutism@users.noreply.github.com>
2021-07-16 22:57:21 -0400

    Add files via upload

---
+++ Doge-Selfbot.plugin.js
@@ -0,0 +1,11 @@
+/**
+ * @name Doge Selfbot
+ * @author Fluffery
+ * @authorId //put here soon
+ * @version 0.1
+ * @description allows you to use a selfbot inside of betterdiscord
+ * @source // copy and paste later
+ * @updateUrl //make one
+ */
+
+m
